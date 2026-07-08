ebf56e7f7d6a63f03f59d600ee05c2aac00a89b6
bonbon148 <townpony608@gmail.com>
2026-07-08 12:31:56 +0300

    ComboBox DONE

--- ComboBox/main.cpp
+++ ComboBox/main.cpp
@@ -1,4 +1,5 @@
 #include<Windows.h>
+#include<cstdio>
 #include"resource.h"
 
 CONST CHAR* g_szVALUES[] = { "This", "is", "my", "Frist", "Combo", "Box" };
@@ -27,6 +28,26 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMag, WPARAM wParam, LPARAM lParam)
 	}
 		break;
 	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			CONST INT SIZE = 256;
+			CHAR szBuffer[SIZE] = {};
+			CHAR szMessage[SIZE] = {};
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+			INT i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);  //Get current selection
+			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)szBuffer);
+			sprintf
+			(
+				szMessage,
+				"Вы выбрали элемент номер N%i со значением '%s'.", i, szBuffer
+			);
+			MessageBox(hwnd, szMessage, "Info", MB_OK | MB_ICONINFORMATION);
+		}
+			break;
+		case IDCANCEL: EndDialog(hwnd, 0);
+		}
 			break;
 	case WM_CLOSE: EndDialog(hwnd, 0);
 		break;
