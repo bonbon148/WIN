be29da2762a04e27e2abbf43e7e11a1ea7b40508
bonbon148 <townpony608@gmail.com>
2026-09-07 10:33:40 +0300

    in progress

--- ListBox/resource.h
+++ ListBox/resource.h
@@ -3,8 +3,8 @@
 // Used by Resource.rc
 //
 #define IDD_DIALOG1                     101
-#define IDC_LIST2                       1002
 #define IDC_LIST_BOX                    1002
+#define IDC_BUTTON1                     1003
 
 // Next default values for new objects
 // 
@@ -12,7 +12,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
